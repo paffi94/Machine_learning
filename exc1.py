9148db945ca0574ba28374466a4577efcab9537a
paffi94 <paffenholz.dirk@web.de>
2020-04-27 23:50:34 +0200

    Aufgabe 1 fertig

--- exc1.py
+++ exc1.py
@@ -1,17 +1,18 @@
-import math, random
 import matplotlib.pyplot as plt
 import numpy as np
 
-x_values = []
-y_values = []
+def generate_random():
+    sample_size = 100
+    x_values = np.sort(np.random.random(sample_size))
+    #random noise
+    epsilon = np.random.uniform(-0.3,0.3)
+    y_values = np.sin(x_values * 2 * np.pi)+epsilon
+    return x_values, y_values
 
-for i in range(100):
-    tmp = random.random()
-    x_values.append(tmp)
-    y_values.append(math.sin(2*math.pi*tmp))
+x, y = generate_random()
 
 
 
 fig, ax = plt.subplots(figsize =(10,6.18))
-ax.plot(x_values, y_values, color='blue')
-fig.savefig("test.pdf")
+ax.plot(x, y, color='blue')
+fig.savefig("A1.pdf")
